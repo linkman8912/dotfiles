08e5d47d3b1f9a0e234579fda58b35436e85aa99
Sam <linkman8912@proton.me>
2024-06-18 12:48:12 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+
+  	description = "Test flake";
+
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-24.05";
+  	};
+
+  	outputs = { self, nixpkgs }:
+		let
+			lib = nixpkgs.lib;
+	in {
+		nixosConfigurations = {
+			nixos = lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [ ./configuration.nix ];
+			};
+		};
+	};
+
+}
