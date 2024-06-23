13879290f36ffdd8d2e7f58227f484d2b1280cf5
Sam <linkman8912@proton.me>
2024-06-23 10:06:10 -0400

    Added device config for mouse

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -68,78 +68,78 @@
 
 			# https://wiki.hyprland.org/Configuring/Variables/#general
 			general { 
-			    gaps_in = 5
-			    gaps_out = 10
+			    	gaps_in = 5
+			    	gaps_out = 10
 
-			    border_size = 2
+			    	border_size = 2
 
-			    # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-			    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-			    col.inactive_border = rgba(595959aa)
+			    	# https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+			    	col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+			    	col.inactive_border = rgba(595959aa)
 
-			    # Set to true enable resizing windows by clicking and dragging on borders and gaps
-			    resize_on_border = false 
+			    	# Set to true enable resizing windows by clicking and dragging on borders and gaps
+			    	resize_on_border = false 
 
-			    # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-			    allow_tearing = false
+			    	# Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+			    	allow_tearing = false
 
-			    layout = dwindle
+			    	layout = dwindle
 			}
 
 			# https://wiki.hyprland.org/Configuring/Variables/#decoration
 			decoration {
-			    rounding = 10
+			    	rounding = 10
 
-			    # Change transparency of focused and unfocused windows
-			    active_opacity = 1
-			    inactive_opacity = 0.8
+			    	# Change transparency of focused and unfocused windows
+			    	active_opacity = 1
+			    	inactive_opacity = 0.8
 
-			    drop_shadow = true
-			    shadow_range = 4
-			    shadow_render_power = 3
-			    col.shadow = rgba(1a1a1aee)
+			    	drop_shadow = true
+			    	shadow_range = 4
+			    	shadow_render_power = 3
+			    	col.shadow = rgba(1a1a1aee)
 
-			    # https://wiki.hyprland.org/Configuring/Variables/#blur
-			    blur {
-				enabled = true
-				size = 3
-				passes = 1
+			    	# https://wiki.hyprland.org/Configuring/Variables/#blur
+			    	blur {
+					enabled = true
+					size = 3
+					passes = 1
 				
-				vibrancy = 0.1696
-			    }
+					vibrancy = 0.1696
+			    	}
 			}
 
 			# https://wiki.hyprland.org/Configuring/Variables/#animations
 			animations {
-			    enabled = true
+			    	enabled = true
 
-			    # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+			    	# Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-			    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-			    animation = windows, 1, 7, myBezier
-			    animation = windowsOut, 1, 7, default, popin 80%
-			    animation = border, 1, 10, default
-			    animation = borderangle, 1, 8, default
-			    animation = fade, 1, 7, default
-			    animation = workspaces, 1, 6, default
+				bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+	
+			    	animation = windows, 1, 7, myBezier
+			    	animation = windowsOut, 1, 7, default, popin 80%
+			    	animation = border, 1, 10, default
+			    	animation = borderangle, 1, 8, default
+			    	animation = fade, 1, 7, default
+			    	animation = workspaces, 1, 6, default
 			}
 
 			# See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
 			dwindle {
-			    pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-			    preserve_split = true # You probably want this
+			    	pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+			    	preserve_split = true # You probably want this
 			}
 
 			# See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
 			master {
-			    new_is_master = true
+			    	new_is_master = true
 			}
 
 			# https://wiki.hyprland.org/Configuring/Variables/#misc
 			misc { 
-			    force_default_wallpaper = 1 # Set to 0 or 1 to disable the anime mascot wallpapers
-			    disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
+			    	force_default_wallpaper = 1 # Set to 0 or 1 to disable the anime mascot wallpapers
+			    	disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
 			}
 
 
@@ -149,20 +149,20 @@
 
 			# https://wiki.hyprland.org/Configuring/Variables/#input
 			input {
-			    kb_layout = us
-			    kb_variant = dvorak
-			    kb_model =
-			    kb_options =
-			    kb_rules =
+			    	kb_layout = us
+			    	kb_variant = dvorak
+			    	kb_model =
+			    	kb_options =
+			    	kb_rules =
 
-			    follow_mouse = 1
+			    	follow_mouse = 1
 
-			    sensitivity = -0.6 # -1.0 - 1.0, 0 means no modification.
-			    accel_profile = "flat"
+			    	sensitivity = -0.6 # -1.0 - 1.0, 0 means no modification.
+			    	accel_profile = "flat"
 
-			    touchpad {
-				natural_scroll = false
-			    }
+				touchpad {
+					natural_scroll = false
+			    	}
 			}
 
 			# https://wiki.hyprland.org/Configuring/Variables/#gestures
@@ -175,6 +175,11 @@
 			device {
 			    name = epic-mouse-v1
 			    sensitivity = -0.5
+			}
+
+			device {
+				name = compx-2.4g-reciever-mouse
+				sensitivity = -0.6
 			}
 
 
