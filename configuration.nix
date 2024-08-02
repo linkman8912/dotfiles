b48476e1ce7c71847c7cbec444b10874b4bc7a39
Sam <linkman8912@proton.me>
2024-08-02 17:12:10 -0400

    IDK

--- configuration.nix
+++ configuration.nix
@@ -150,8 +150,10 @@
   	environment.systemPackages = with pkgs; [
 		vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 		wget
+		handbrake
   		vesktop
   		xautoclick
+		vlc
 		prismlauncher
   		pkgs.spotify
 		kitty
