5f556131d6d1e5dd597280110e63d9f48440a315
linkman <linkman8912@proton.me>
2024-08-24 19:36:00 -0400

    added plover

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,7 @@
 		vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 		wget
 		handbrake
+		plover.dev
   		vesktop
   		xautoclick
 		pamixer
