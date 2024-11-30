d3d977b1df29453cba48e9f5a115f2932a0c13bd
linkman <linkman8912@proton.me>
2024-11-30 16:30:07 -0500

    fix home-manager after switching to unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager/release-24.05";
+		home-manager.url = "github:nix-community/home-manager/master";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 		hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 		stylix.url = "github:danth/stylix/release-24.05";
