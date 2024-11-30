928d9407b91a393ca405238f0464ace3547beae6
linkman <linkman8912@proton.me>
2024-11-30 16:49:32 -0500

    change home-manager/master to home-manager

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager/master";
+		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 		hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 		stylix.url = "github:danth/stylix/release-24.05";
