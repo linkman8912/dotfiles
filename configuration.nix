8d73d4aee8da39a6016d35af9cdbf146d83d8701
linkman <linkman8912@proton.me>
2025-01-30 17:17:08 -0500

    added everest-mons, we speedrunning

--- configuration.nix
+++ configuration.nix
@@ -164,6 +164,27 @@
         KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
       '';
     };
+    printing.drivers = [ 
+	  pkgs.gutenprint # — Drivers for many different printers from many different vendors.
+	  pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
+	  pkgs.hplip # — Drivers for HP printers.
+	  pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
+	  pkgs.postscript-lexmark # — Postscript drivers for Lexmark
+	  pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
+	  pkgs.splix # — Drivers for printers supporting SPL (Samsung Printer Language).
+	  pkgs.brlaser # — Drivers for some Brother printers
+	  pkgs.brgenml1lpr #  — Generic drivers for more Brother printers [1]
+	  pkgs.brgenml1cupswrapper  # — Generic drivers for more Brother printers [1]
+	  pkgs.cnijfilter2 # — Drivers for some Canon Pixma devices (Proprietary driver)
+      pkgs.canon-cups-ufr2
+      pkgs.cups-bjnp
+	]; 
+    
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
   #programs.ssh.askPassword = "${pkgs.gnome.seahorse}/libexec/seahorse/ssh-askpass";
   programs.fish.enable = true;
@@ -173,6 +194,7 @@
 
   	# Enable CUPS to print documents.
   	services.printing.enable = true;
+
 
   	# Enable sound with pipewire.
   	hardware.pulseaudio.enable = false;
@@ -240,6 +262,7 @@
       arp-scan
       networkmanager
 	  brightnessctl
+      everest-mons
       vial
       cider
 	  git-lfs
