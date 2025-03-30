7b833abd669e8ab6964eeee1cbcbe6ce81560300
Sam <linkman8912@proton.me>
2025-03-30 12:06:10 -0400

    added vimwiki

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -5,6 +5,12 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set autoindent")
 vim.cmd("set expandtab")
+vim.cmd("set nocompatible")
+vim.cmd("filetype plugin on")
+vim.cmd("syntax on")
 -- Reserve a space in the gutter
 -- This will avoid an annoying layout shift in the screen
 vim.opt.signcolumn = "yes"
+
+-- Change vimwiki syntak to markdown
+vim.cmd("let g:vimwiki_list = [{'path': '~/vimwiki/',  'syntax': 'markdown', 'ext': 'md'}]")
