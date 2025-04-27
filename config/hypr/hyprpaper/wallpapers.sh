d8d5bb4c86f8333dbcced9e8af3bc412238249ed
linkman <linkman8912@proton.me>
2025-04-27 12:16:16 -0400

    changed .wallpapers to .config/wallpapers

--- config/hypr/hyprpaper/wallpapers.sh
+++ config/hypr/hyprpaper/wallpapers.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env fish
-mkdir ~/.wallpapers
-if test -b "~/.wallpapers/night-city.gif"
+mkdir $XDG_CONFIG_HOME/wallpapers
+if test -b "$XDG_CONFIG_HOME/wallpapers/night-city.gif"
 	echo "night-city.gif exists"
 else
-	ffmpeg -i ~/.dotfiles/config/hypr/hyprpaper/night-city.mp4 -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 ~/.wallpapers/night-city.gif
+	ffmpeg -i ~/.dotfiles/config/hypr/hyprpaper/night-city.mp4 -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 $XDG_CONFIG_HOME/wallpapers/night-city.gif
 	echo "night-city.gif created"
 end
-if test -b "~/.wallpapers/pixel-car.gif"
+if test -b "$XDG_CONFIG_HOME/wallpapers/pixel-car.gif"
 	echo "pixel-car.gif exists"
 else
-	ffmpeg -i ~/.dotfiles/config/hypr/hyprpaper/pixel-car.mp4 -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 ~/.wallpapers/pixel-car.gif
+	ffmpeg -i ~/.dotfiles/config/hypr/hyprpaper/pixel-car.mp4 -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 $XDG_CONFIG_HOME/wallpapers/pixel-car.gif
 	echo "pixel-car.gif created"
 end
-if test -b "~/.wallpapers/goodmorningnightcity.webm"
+if test -b "$XDG_CONFIG_HOME/wallpapers/goodmorningnightcity.webm"
 	echo "goodmorningnightcity.gif exists"
 else
-    yt-dlp 'https://youtu.be/srtbmP_WJgY?si=oewz2REjOP33oLp3' --output=~/.wallpapers/goodmorningnightcity.webm
+    yt-dlp 'https://youtu.be/srtbmP_WJgY?si=oewz2REjOP33oLp3' --output=$XDG_CONFIG_HOME/wallpapers/goodmorningnightcity.webm
 	echo "goodmorningnightcity.gif created"
 end
-if test -b "~/.wallpapers/goodmorningnightcity.gif"
+if test -b "$XDG_CONFIG_HOME/wallpapers/goodmorningnightcity.gif"
 	echo "goodmorningnightcity.gif exists"
 else
-	ffmpeg -i '~/.wallpapers/goodmorningnightcity.webm' -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 ~/.wallpapers/goodmorningnightcity.gif
+	ffmpeg -i '$XDG_CONFIG_HOME/wallpapers/goodmorningnightcity.webm' -vf "fps=30,scale=1920:-1:flags=lanczos" -loop 0 $XDG_CONFIG_HOME/wallpapers/goodmorningnightcity.gif
 	echo "goodmorningnightcity.gif created"
 end
 echo "ok"
