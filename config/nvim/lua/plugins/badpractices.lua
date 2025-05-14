a35568239cebd0ef9670f575037c939b1503df8c
linkman <linkman8912@proton.me>
2025-05-13 20:54:02 -0400

    nvim: change badpractices to be slightly more lenient

--- config/nvim/lua/plugins/badpractices.lua
+++ config/nvim/lua/plugins/badpractices.lua
@@ -4,7 +4,7 @@ return {
     require("bad_practices").setup({
       most_splits = 3, -- how many splits are considered a good practice(default: 3)
       most_tabs = 3, -- how many tabs are considered a good practice(default: 3)
-      max_hjkl = 10, -- how many times you can spam hjkl keys in a row(default: 10)
+      max_hjkl = 20, -- how many times you can spam hjkl keys in a row(default: 10)
     })
   end
 }
