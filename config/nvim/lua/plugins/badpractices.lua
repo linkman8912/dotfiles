dcdcb87f3b094e7e16399d8d8f6dc29951c8b292
linkman <linkman8912@proton.me>
2025-05-20 11:14:02 -0400

    nvim: disable badpractices

--- config/nvim/lua/plugins/badpractices.lua
+++ config/nvim/lua/plugins/badpractices.lua
@@ -1,10 +1,10 @@
 return {
   "antonk52/bad-practices.nvim",
   config = function()
-    require("bad_practices").setup({
-      most_splits = 3, -- how many splits are considered a good practice(default: 3)
-      most_tabs = 3, -- how many tabs are considered a good practice(default: 3)
-      max_hjkl = 20, -- how many times you can spam hjkl keys in a row(default: 10)
-    })
+    --require("bad_practices").setup({
+      --most_splits = 3, -- how many splits are considered a good practice(default: 3)
+      --most_tabs = 3, -- how many tabs are considered a good practice(default: 3)
+      --max_hjkl = 20, -- how many times you can spam hjkl keys in a row(default: 10)
+    --})
   end
 }
