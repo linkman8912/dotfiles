2e2f6c1fa2070ed556f351e8dc4161e8f3e5acb9
linkman <linkman8912@proton.me>
2025-06-14 20:47:47 -0400

    various updates

---
+++ config/nvim/lua/plugins/roslyn.lua
@@ -0,0 +1,26 @@
+return {
+  "seblyng/roslyn.nvim",
+  ft = "cs",
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+  config = function()
+    -- Add other options here
+    vim.lsp.config("roslyn", {
+      on_attach = function()
+        print("roslyn attached")
+      end,
+      settings = {
+        ["csharp|inlay_hints"] = {
+          csharp_enable_inlay_hints_for_implicit_object_creation = true,
+          csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        },
+        ["csharp|code_lens"] = {
+          dotnet_enable_references_code_lens = true,
+        },
+      },
+    })
+  end,
+}
