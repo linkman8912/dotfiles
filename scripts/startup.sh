396afe83fbd297e42500a6bee4ce6842627e1a54
linkman <linkman8912@proton.me>
2025-09-17 09:57:09 -0400

    update hyprland.conf to work with the new hyprland touch gesture system

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 kanata -n & disown
-/home/linkman/.dotfiles/config/hypr/hyprpaper/wallpapers.sh & disown
+#/home/linkman/.dotfiles/config/hypr/hyprpaper/wallpapers.sh & disown
 mpd & disown
 mpDris2 & disown
 yams -d & disown
